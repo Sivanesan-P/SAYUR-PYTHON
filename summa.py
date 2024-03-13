a7158ad9afccc0a36c61c39d5375f9b8f2327a22
Sivanesan <sivanesan4123@gmail.com>
2024-03-13 15:41:43 +0530

    summa

--- summa.py
+++ summa.py
@@ -1,69 +1,36 @@
-# User
-# Parking fee is 
-# If you return within 15 mins, its free
-# Rs 100 for the first hr
-# Rs 150 for each hr after that. 
-# Fee is calculated in 30 min increments. (meaning, if you spent 25 mins, you will be charged for 30 mins
-# If you spend 35 mins, you will be charged for one hr)
-# Get entry time and exit time from customer as input and display the fee
-
-
 def parse_time(time_str):
     hours, minutes = map(int, time_str.split(':'))
     return hours, minutes
 
 def payment():
-    global cost
-    amount=0
-    entry_time_str =input("Enter the entry time (HH:MM): ")
+    entry_time_str = input("Enter the entry time (HH:MM): ")
     exit_time_str = input("Enter the exit time (HH:MM): ")
+
+    entry_hours, entry_minutes = parse_time(entry_time_str)
     exit_hours, exit_minutes = parse_time(exit_time_str)
 
-   
-    entry_hours, entry_minutes = parse_time(entry_time_str)
+    total_minutes = (exit_hours - entry_hours) * 60 + (exit_minutes - entry_minutes)
+    total_hrs = total_minutes // 60
+    extra_min = total_minutes % 60
 
-    exit_total_minutes = exit_hours * 60 + exit_minutes
-    entry_total_minutes = entry_hours * 60 + entry_minutes
-
-    total_minutes = exit_total_minutes - entry_total_minutes
-    total_hrs=total_minutes//60
-    extra_min=total_minutes-total_hrs*60
-    print("hr : ",total_hrs)
-    print("min : ", extra_min)
-
-   
-    if extra_min>30:
-        extraamt=100
-    elif extra_min<=30:
-        extraamt=50
-    
-    if total_hrs==0:
-        if extra_min<=15:
-            return 0
-    elif total_hrs==1:
-        amount=100
-        if extra_min<=30:
-            amount+=50
-        else:
-            amount+=150
-            return amount
-        return amount
-    elif total_hrs==2:
-        amount=100+150
-        if extra_min<=30:
-            amount+=50
-        else:
-            amount+=150
-            return amount
-        return amount
+    if extra_min > 30:
+        extra_amt = 100
     else:
-         amount=100+150*total_hrs-2
-         if extra_min<=30:
-            amount+=50
-         else:
-            amount+=150
-            return amount
-         return amount
+        extra_amt = 50
 
-    
+    if total_hrs == 0:
+        if extra_min <= 15:
+            return 0
+        else:
+            return extra_amt
+    elif total_hrs == 1:
+        amount = 100 + extra_amt
+    else:
+        amount = 100 + 150 * (total_hrs - 1) + extra_amt
+
+    if extra_min > 30:
+        amount += 150
+
+    return amount
+
 print(payment())
